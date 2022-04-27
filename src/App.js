6b07afc50bb30aba2ed2421e5648d973602c0aaa
apatel544 <ankurpatel544@gmail.com>
2022-04-27 12:03:57 -0400

    Adding files to formik project

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+// TODO: import useFormik from formik library
+import { useFormik } from "formik";
+
+function App() {
+  // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values): void => {
+      alert("Login Successful");
+    },
+  });
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Email:</div>
+        <input
+          id="emailField"
+          type="text"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        {formik.errors.email ? (
+          <div id="emailError" style={{ color: "red" }}>
+            {formik.errors.email}
+          </div>
+        ) : null}
+        <div>Password:</div>
+        <input
+          id="pswField"
+          type="text"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        <br />
+        {formik.errors.password ? (
+          <div id="pswError" style={{ color: "red" }}>
+            {formik.errors.password}
+          </div>
+        ) : null}
+        <button id="submitBtn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
